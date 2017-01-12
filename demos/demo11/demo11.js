b5eb1484b49cba7b026e6f3ed66195206ae4a397
24wings <2121718893@qq.com>
2017-01-12 12:44:10 +0800

    demo12

--- demos/demo11/demo11.js
+++ demos/demo11/demo11.js
@@ -2,23 +2,39 @@ $(function() {
     // 所有轮播
     var $carouselItems = $('#carousel .item');
     var activeItem = 0;
-    setInterval(function() {
-        // 超过轮播数量,自动重置
-        if (activeItem === $carouselItems.length - 1) {
-            $carouselItems.hide().first().show();
-            $carouselItems.last().show();
-            $($carouselItems.get(0)).css({ 'margin-left': '-600px' });
-            $($carouselItems.get(0)).animate({ marginLeft: '0px' }, 500, function() {
-                $carouselItems.show();
-                activeItem++;
-            });
-        } else {
 
+    function carousel() {
+        return setInterval(function() {
+            // 超过轮播数量,自动重置
+            if (activeItem === $carouselItems.length - 1) {
+                $carouselItems.hide().first().show();
+                $carouselItems.last().show();
+                $($carouselItems.get(0)).css({ 'margin-left': '-600px' });
+                $($carouselItems.get(0)).animate({ marginLeft: '0px' }, 500, function() {
+                    $carouselItems.show();
+                    activeItem++;
+                });
+            } else {
+                activeItem = activeItem > $carouselItems.toArray().length - 2 ? 0 : ++activeItem;
+                $($carouselItems.get(0)).animate({ marginLeft: '-' + activeItem * 600 + 'px' }, 500);
 
-            activeItem = activeItem > $carouselItems.toArray().length - 2 ? 0 : ++activeItem;
-            $($carouselItems.get(0)).animate({ marginLeft: '-' + activeItem * 600 + 'px' }, 500);
-        }
-    }, 1000);
+            }
+        }, 1000);
+    }
+
+    var timmer = carousel();
+    $('#carousel .navigation li').mouseenter(function() {
+        // 鼠标进入,停止轮播
+        clearInterval(timmer);
+        // 移入小点点的下标
+        var index = $('#carousel .navigation li').index($(this));
+        $(this).addClass('active');
+        activeItem = index;
+
+        $($carouselItems.get(0)).animate({ marginLeft: '-' + activeItem * 600 + 'px' }, 500);
+    }).mouseleave(function() {
+
+    });
 
 
 });
---
+++ index.js
@@ -0,0 +1,6 @@
+$(function() {
+    var demos = 12;
+    for (var i = 0; i < demos; i++) {
+        $('.demos').append(`<li></li><a href="demos/demo${i+1}/demo${i+1}.html" >demo${i+1}</a></li>`);
+    }
+});
